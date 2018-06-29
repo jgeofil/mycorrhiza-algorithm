8b3c1ff601aa7aba28ed4c1923a32f5b5a9f1ed6
jgeofil <jeremy.geo@gmail.com>
2018-06-29 07:39:36 -0400

    package init

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mycorrhiza",
+    version="0.0.1",
+    author="Jeremy Georges-Filteau",
+    author_email="jeremy.geo@gmail.com",
+    description="Mycorrhiza population assignment tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jgeofil/mycorrhiza",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)Close",
+        "Operating System :: OS Independent",
+    ),
+)
