a6302e3992a4c15039ba15ba565833e53f17d9fe
jgeofil <jeremy.geo@gmail.com>
2018-07-22 14:56:16 -0400

    path fix

--- mycorrhiza/settings.py
+++ mycorrhiza/settings.py
@@ -5,9 +5,9 @@ os_name = platform.system()
 linux_path = '~/splitstree4/SplitsTree'
 mac_path = '/Applications/SplitsTree/SplitsTree.app/Contents/MacOS/JavaApplicationStub'
 
-if os_name is 'Linux':
+if os_name == 'Linux':
 	p = linux_path
-elif os_name is 'Darwin':
+elif os_name == 'Darwin':
 	p = mac_path
 else:
 	p = linux_path
